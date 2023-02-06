b26f101212c05fe6af4c20070330f2fe9490d21d
Prakash Thite <prakashthite1@gmail.com>
2023-02-06 16:44:17 +0000

    added accessmodifier code in github repository

---
+++ com/Accessmodifier/PublicAccessModifierExample.java
@@ -0,0 +1,21 @@
+package com.Accessmodifier;
+
+public class PublicAccessModifierExample {
+	
+	//public access modified can provide the accessibility level inside the class,
+			//outside the class to other classes in the same package or the other package as well
+			//It is the highest level accessibility in the java
+			
+			// Variable,methods and Constructor
+			//Constructor can be Default or Not Parameterized or they can be Parameterized Constructor
+	
+	public static void main(String[] args)
+	{
+		
+		int value1 = 20;
+		int value2 = 30;
+		
+		System.out.println("The addition of value1 and value2 :: " + (value1+value2));
+	}
+
+}
